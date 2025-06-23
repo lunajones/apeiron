84f8fd5ca2eacca2e5e6e16dcc6250b26ea14bc7
elmir <elmirsnascimento@gmail.com>
2025-06-22 23:01:40 -0300

    adjusting for first run simulation

--- service/ai/core/behavior_node.go
+++ service/ai/core/behavior_node.go
@@ -2,14 +2,6 @@ package core
 
 import "github.com/lunajones/apeiron/service/creature"
 
-type BehaviorStatus int
-
-const (
-	StatusSuccess BehaviorStatus = iota
-	StatusFailure
-	StatusRunning
-)
-
 type BehaviorNode interface {
 	Tick(c *creature.Creature, ctx AIContext) BehaviorStatus
 }
--- service/ai/core/sequence.go
+++ service/ai/core/sequence.go
@@ -6,9 +6,9 @@ type SequenceNode struct {
 	Children []BehaviorNode
 }
 
-func (n *SequenceNode) Tick(c *creature.Creature) BehaviorStatus {
-	for _, child := range n.Children {
-		status := child.Tick(c)
+func (s *SequenceNode) Tick(c *creature.Creature, ctx AIContext) BehaviorStatus {
+	for _, child := range s.Children {
+		status := child.Tick(c, ctx)
 		if status != StatusSuccess {
 			return status
 		}
