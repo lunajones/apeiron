77c5f57438b9dfc8a4591d0f8068bfb4cb00b5af
elmir <elmirsnascimento@gmail.com>
2025-06-23 00:13:46 -0300

    adjusting for first run simulation

--- main.go
+++ main.go
@@ -6,18 +6,25 @@ import (
 
 	"github.com/lunajones/apeiron/service/ai"
 	"github.com/lunajones/apeiron/lib/combat"
-	"github.com/lunajones/apeiron/service/creature"
 	"github.com/lunajones/apeiron/service/world"
+	"github.com/lunajones/apeiron/service/zone"
 )
 
 func main() {
-	log.Println("[Server] Iniciando servidor de zona...")
+	log.Println("[Main] Inicializando sistema...")
 
-	creature.Init()
+	// Inicializa as skills
 	combat.InitSkills()
-	ai.InitBehaviorTrees()
+
+	// Inicializa as zonas e criaturas
 	zone.Init()
 
+	// Inicializa as árvores de comportamento para as criaturas da primeira zona
+	if len(zone.Zones) > 0 {
+		ai.InitBehaviorTrees(world.Players, zone.Zones[0].Creatures)
+	}
+
+	// Loop de Tick a cada 50ms (~20 ticks por segundo)
 	ticker := time.NewTicker(50 * time.Millisecond)
 	defer ticker.Stop()
 
